f033e7001876875ac020089f6bdd3e058d12b814
yashj29 <29yashjain29@gmail.com>
2025-05-29 11:55:04 +0530

    About section Completed

--- components/About.jsx
+++ components/About.jsx
@@ -1,4 +1,4 @@
-import { assets, infoList } from '@/assests/assets'
+import { assets, infoList, toolsData } from '@/assests/assets'
 import Image from 'next/image'
 import React from 'react'
 
@@ -17,13 +17,24 @@ const About = () => {
 
             <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
                 {infoList.map(({icon, iconDark, title, description}, index)=>(
-                    <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer' key={index}>
+                    <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500 hover:shadow-[4px_4px_0_#000]' key={index}>
                         <Image src={icon} alt={title} className='w-7 mt-3' />
                         <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
                         <p className='text-gray-600 text-sm'>{description}</p>
 
                     </li>
                 ))}
+            </ul>
+
+            <h4 className='my-6 text-gray-700 font-Ovo'>Tools I use</h4>
+            <ul className='flex items-center gap-3 sm:gap-5'>
+              {toolsData.map((tool, index)=>(
+                <li className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500' 
+                key={index}>
+                  <Image src={tool} alt='Tool' className='w-5 sm:w-7'/>
+                </li>
+                
+              ))}
             </ul>
         </div>
       </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,10 @@
 import { assets } from '@/assests/assets'
 import Image from 'next/image'
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Navbar = () => {
 
+  const [isScroll, setIsScroll] = useState(false);
   const sideMenuRef = useRef();
 
   const openMenu = () => {
@@ -13,16 +14,26 @@ const Navbar = () => {
     sideMenuRef.current.style.transform = 'translateX(16rem)'
   }
 
+  useEffect(()=>{
+    window.addEventListener('scroll', () => {
+        if(scrollY > 50){
+          setIsScroll(true);
+        }else{
+          setIsScroll(false);
+        }
+    })
+  }, [])
+
   return (
     <>
     <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
       <Image src={assets.header_bg_color} alt='' className='w-full'/>
     </div>
-    <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+    <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""}`}>
         <a href="#top">
             <Image src={assets.logo} alt="" className='w-28 cursor-pointer mr-14'/>
         </a>
-        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
+        <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50"}`}>
             <li><a href="#top">Home</a></li>
             <li><a href="#about">About me</a></li>
             <li><a href="#services">Services</a></li>
