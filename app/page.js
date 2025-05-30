485b103444c3d5194908fd810612a07ecf9ec163
yashj29 <29yashjain29@gmail.com>
2025-05-30 21:04:29 +0530

    Contact page completed

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 'use client'
 import About from "@/components/About";
+import Contact from "@/components/Contact";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Services from "@/components/Services";
@@ -13,6 +14,7 @@ export default function Home() {
     <About />
     <Services />
     <Work />
+    <Contact />
     </>
   );
 }
---
+++ components/Contact.jsx
@@ -0,0 +1,53 @@
+import { assets } from '@/assests/assets'
+import Image from 'next/image'
+import React, { useState } from 'react'
+
+const Contact = () => {
+
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "fb53af42-fe40-408e-b142-081d9addd299");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <div id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]'>
+      <h4 className='text-center mb-2 text-lg font-Ovo'>Connect with me</h4>
+      <h2 className='text-center text-5xl font-Ovo'>Get in touch</h2>
+      <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+        I'd love to hear from you! If you have any questions, comments, or feedback, please use the form below.
+      </p>
+
+      <form onSubmit={onSubmit} className='max-w-2xl mx-auto'>
+        <div className='grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-6 mt-10 mb-8'>
+          <input type="text" placeholder='Enter your name' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' name='name' />
+          <input type="email" placeholder='Enter your email' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' name='email' />
+        </div>
+        <textarea rows='6' placeholder='Enter your message' required className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6' name='message'></textarea>
+        <button type='submit' className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500'>Submit now <Image src={assets.right_arrow_white} alt='' className='w-4' /></button>
+        <p className='mt-4'>{result}</p>
+      </form>
+    </div>
+  )
+}
+
+export default Contact
