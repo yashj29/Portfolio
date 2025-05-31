f26d622c31ef109656c797080116483f820d61a6
yashj29 <29yashjain29@gmail.com>
2025-05-31 19:41:10 +0530

    Footer completed

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 'use client'
 import About from "@/components/About";
 import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Services from "@/components/Services";
@@ -15,6 +16,7 @@ export default function Home() {
     <Services />
     <Work />
     <Contact />
+    <Footer />
     </>
   );
 }
