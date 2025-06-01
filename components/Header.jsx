677c270b736ce4baff61117498057694a35244e0
yashj29 <29yashjain29@gmail.com>
2025-06-01 14:39:42 +0530

    DarkMode Header completed

--- components/Header.jsx
+++ components/Header.jsx
@@ -20,10 +20,10 @@ const Header = () => {
             </p>
             <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
                 <a href="#contact"
-                className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>
+                className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>
                     contact me <Image src={assets.right_arrow_white} alt='' className='w-4' /></a>
                 <a href="/sample-resume.pdf" download
-                className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>
+                className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>
                     my resume <Image src={assets.download_icon} alt='' className='w-4' /></a>
             </div>
         </div>
