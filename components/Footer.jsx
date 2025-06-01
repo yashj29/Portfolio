5338c7b271d15ae7b56ff45cd6aa9b5aae780a73
yashj29 <29yashjain29@gmail.com>
2025-06-01 22:56:35 +0530

    DarkMode Contact and Footer completed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,13 +2,13 @@ import { assets } from '@/assests/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const Footer = () => {
+const Footer = ({isDarkMode}) => {
   return (
     <div className='mt-20'>
       <div className='text-center'>
-        <Image src={assets.logo} alt='' className='w-36 mx-auto mb-2'/>
+        <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-36 mx-auto mb-2'/>
         <div className='w-max flex items-center gap-2 mx-auto'>
-            <Image src={assets.mail_icon} alt='' className='w-6'/>
+            <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6'/>
             yashj2k03@gmail.com
         </div>
       </div>
