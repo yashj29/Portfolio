81d0f555937d95d058b03a36272e615a4c7df55c
yashj29 <29yashjain29@gmail.com>
2025-06-01 22:00:42 +0530

    DarkMode Work completed

--- components/Work.jsx
+++ components/Work.jsx
@@ -2,7 +2,7 @@ import { assets, workData } from '@/assests/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const Work = () => {
+const Work = ({isDarkMode}) => {
     return (
         <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
             <h4 className='text-center mb-2 text-lg font-Ovo'>My portfolio</h4>
@@ -11,9 +11,9 @@ const Work = () => {
                 I am a frontend web developer with a passion for creating beautiful and functional websites. I have experience in HTML, CSS, JavaScript, and React, and I am always eager to learn new technologies and improve my skills. Here are some of the services I offer:
             </p>
 
-            <div className='grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] my-10 gap-5'>
+            <div className='grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] my-10 gap-5 dark:text-black'>
                 {workData.map((project, index) => (
-                    <div key={index}
+                    <div key={index} 
                         className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'
                         style={{ backgroundImage: `url(${project.bgImage})` }}>
                         <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
@@ -29,8 +29,8 @@ const Work = () => {
                 ))}
             </div>
 
-            <a href="" className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-[#fcf4ff] duration-500'>
-                Show more <Image src={assets.right_arrow_bold} alt='Right arrow' className='w-4' />
+            <a href="" className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-[#fcf4ff] duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover'>
+                Show more <Image src={isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold} alt='Right arrow' className='w-4' />
             </a>
         </div>
     )
