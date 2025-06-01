1a9e833ea8415f6e8a8a90c5b6f92aa91f32da3c
yashj29 <29yashjain29@gmail.com>
2025-06-01 17:44:40 +0530

    DarkMode Services completed

--- components/Services.jsx
+++ components/Services.jsx
@@ -13,10 +13,10 @@ const Services = () => {
 
             <div className='grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-6 my-10'>
                 {serviceData.map(({icon, title, description, link}, index)=>(
-                    <div key={index} className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-[4px_4px_0_#000] cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500'>
+                    <div key={index} className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-[4px_4px_0_#000] cursor-pointer hover:bg-[#fcf4ff] hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white'>
                         <Image src={icon} alt='' className='w-10'/>
-                        <h3 className='text-lg my-4 text-gray-700'>{title}</h3>
-                        <p className='text-sm text-gray-600 leading-5'>{description}</p>
+                        <h3 className='text-lg my-4 text-gray-700 dark:text-white'>{title}</h3>
+                        <p className='text-sm text-gray-600 leading-5 dark:text-white/80'>{description}</p>
                         <a href={link} className='flex items-center gap-2 text-sm mt-5'>
                             Read more <Image alt='' src={assets.right_arrow} className='w-4'/>
                         </a>
