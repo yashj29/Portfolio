b5e29137c10bc0147025a5ce62cdc3aad4646529
yashj29 <29yashjain29@gmail.com>
2025-06-02 17:54:30 +0530

    Work using Framer motion

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,19 +1,46 @@
 import { assets, workData } from '@/assests/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from "motion/react"
 
 const Work = ({isDarkMode}) => {
     return (
-        <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
-            <h4 className='text-center mb-2 text-lg font-Ovo'>My portfolio</h4>
-            <h2 className='text-center text-5xl font-Ovo'>My latest work</h2>
-            <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
-                I am a frontend web developer with a passion for creating beautiful and functional websites. I have experience in HTML, CSS, JavaScript, and React, and I am always eager to learn new technologies and improve my skills. Here are some of the services I offer:
-            </p>
+        <motion.div 
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        transition={{duration: 1}}
+        id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
 
-            <div className='grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] my-10 gap-5 dark:text-black'>
+            <motion.h4 
+            initial={{y: -20, opacity: 0}}
+            whileInView={{y: 0, opacity: 1}}
+            transition={{delay: 0.3, duration: 0.5}}
+            className='text-center mb-2 text-lg font-Ovo'>My portfolio</motion.h4>
+
+            <motion.h2 
+            initial={{y: -20, opacity: 0}}
+            whileInView={{y: 0, opacity: 1}}
+            transition={{delay: 0.5, duration: 0.5}}
+            className='text-center text-5xl font-Ovo'>My latest work</motion.h2>
+
+            <motion.p 
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1}}
+            transition={{delay: 0.7, duration: 0.5}}
+            className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+                I am a frontend web developer with a passion for creating beautiful and functional websites. I have experience in HTML, CSS, JavaScript, and React, and I am always eager to learn new technologies and improve my skills. Here are some of the services I offer:
+            </motion.p>
+
+            <motion.div 
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1}}
+            transition={{delay: 0.9, duration: 0.6}}
+            className='grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] my-10 gap-5 dark:text-black'>
                 {workData.map((project, index) => (
-                    <div key={index} 
+                    <motion.div 
+                    whileHover={{scale: 1.05}}
+                    transition={{duration: 0.3}}
+                    key={index} 
                         className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'
                         style={{ backgroundImage: `url(${project.bgImage})` }}>
                         <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7'>
@@ -25,14 +52,18 @@ const Work = ({isDarkMode}) => {
                                 <Image src={assets.send_icon} alt='send icon' className='w-5' />
                             </div>
                         </div>
-                    </div>
+                    </motion.div>
                 ))}
-            </div>
+            </motion.div>
 
-            <a href="" className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-[#fcf4ff] duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover'>
+            <motion.a 
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1}}
+            transition={{delay: 1.1, duration: 0.5}}
+            href="" className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-[#fcf4ff] duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover'>
                 Show more <Image src={isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold} alt='Right arrow' className='w-4' />
-            </a>
-        </div>
+            </motion.a>
+        </motion.div>
     )
 }
 
