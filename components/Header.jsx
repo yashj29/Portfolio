33eebded4949cc055c7aa69b3a21a6f04aa6c050
yashj29 <29yashjain29@gmail.com>
2025-06-02 11:13:44 +0530

    Header using Framer motion

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,30 +1,59 @@
 import { assets } from '@/assests/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from "motion/react"
 
 const Header = () => {
     return (
         <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
-            <div>
+            <motion.div
+            initial={{scale: 0}}
+            whileInView={{scale: 1}}
+            transition={{duration: 0.8, type: 'spring', stiffness: 100}}
+            >
                 <Image src={assets.profile_img} alt='' className='rounded-full w-32' />
-            </div>
-            <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>
+            </motion.div>
+            <motion.h3 
+            initial={{y: -20, opacity: 0}}
+            whileInView={{y: 0, opacity: 1}}
+            transition={{duration: 0.6, delay: 0.3}}
+            className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>
                 Hi! I'm Yash Jain <Image src={assets.hand_icon} alt='' className='w-6' />
-            </h3>
+            </motion.h3>
 
-            <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>
+            <motion.h1 
+            initial={{y: -30, opacity: 0}}
+            whileInView={{y: 0, opacity: 1}}
+            transition={{duration: 0.8, delay: 0.5}}
+            className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>
                 Frontend web developer
-            </h1>
-            <p className='max-w-2xl mx-auto font-Ovo'>
+            </motion.h1>
+            <motion.p 
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1}}
+            transition={{duration: 0.6, delay: 0.7}}
+            className='max-w-2xl mx-auto font-Ovo'>
                 I am a frontend web developer with a passion for creating beautiful and functional websites. I have experience in HTML, CSS, JavaScript, and React, and I am always eager to learn new technologies and improve my skills.
-            </p>
+            </motion.p>
+
             <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
-                <a href="#contact"
+
+                <motion.a 
+                initial={{y: 30, opacity: 0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{duration: 0.6, delay: 1}}
+                href="#contact"
                 className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>
-                    contact me <Image src={assets.right_arrow_white} alt='' className='w-4' /></a>
-                <a href="/sample-resume.pdf" download
+                    contact me <Image src={assets.right_arrow_white} alt='' className='w-4' /></motion.a>
+
+                <motion.a 
+                initial={{y: 30, opacity: 0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{duration: 0.6, delay: 1.2}}
+                href="/sample-resume.pdf" download
                 className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>
-                    my resume <Image src={assets.download_icon} alt='' className='w-4' /></a>
+                    my resume <Image src={assets.download_icon} alt='' className='w-4' /></motion.a>
+
             </div>
         </div>
     )
